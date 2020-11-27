8547bd0f7998a58993e729aefc1b81bdc24e0b5a
English-Ghost <56802968+English-Ghost@users.noreply.github.com>
2020-11-27 01:02:19 -0600

    updated files for nav
    navagation bar updates and new js written

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/index.js';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <h1> hellos </h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
