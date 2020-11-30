f744bb3e372dfc0be6343a3cb702b45e4e36d9c6
English-Ghost <adamccastell@gmail.com>
2020-11-30 17:38:33 -0600

    added footer
    video ended at 3:12:30 where the social media icons came into play. I have not coded the actual elements yet

--- website/src/components/Services/index.js
+++ website/src/components/Services/index.js
@@ -15,22 +15,22 @@ import {
 const Services = () => {
   return (
     <ServicesContainer id="services">
-      <ServicesH1> </ServicesH1>
+      <ServicesH1>Our Services</ServicesH1>
       <ServicesWrapper>
         <ServicesCard>
           <ServicesIcon src={Icon1} />
           <ServicesH2>Reduce Expenses</ServicesH2>
-          <ServicesP>We help reduce your fees and increase your overall revenue</ServicesP>
+          <ServicesP>We help reduce your fees and increase your overall revenue.</ServicesP>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Icon2} />
           <ServicesH2>Virtual Offices</ServicesH2>
-          <ServicesP>You can acces our platform from anywhere in the world</ServicesP>
+          <ServicesP>You can acces our platform online anywhere in the world.</ServicesP>
         </ServicesCard>
         <ServicesCard>
           <ServicesIcon src={Icon3} />
           <ServicesH2>Premium Benefits</ServicesH2>
-          <ServicesP>We help reduce your fees and increase your overall revenue</ServicesP>
+          <ServicesP>We help reduce your fees and increase your overall revenue.</ServicesP>
         </ServicesCard>
       </ServicesWrapper>
     </ServicesContainer>
